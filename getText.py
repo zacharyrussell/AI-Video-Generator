ca7e590cbae9f411eb8a0f377f52a6b4d27314e4
zacharyrussell <106686403+zacharyrussell@users.noreply.github.com>
2023-04-08 13:36:34 -0400

    Update getText.py

--- getText.py
+++ getText.py
@@ -2,7 +2,7 @@ import openai
 
 
 def getText(prompt, tokens):
-    openai.api_key = "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"
+    openai.api_key = "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" #disabled key
 
     response = openai.Completion.create(model="text-davinci-003", prompt=prompt, temperature=0, max_tokens=tokens)
     # print(response.get('choices'))
